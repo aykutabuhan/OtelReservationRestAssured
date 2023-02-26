ed7f97aaeb0c0a3eebcc190565135723efaba26a
Aykut ABUHAN <abuhanaykut@yahoo.com>
2023-02-26 15:47:08 +0300

    Created PropertyUtil Class

---
+++ src/main/java/util/PropertyUtil.java
@@ -0,0 +1,35 @@
+package util;
+
+import java.io.*;
+import java.util.Properties;
+
+public class PropertyUtil {
+
+    private static final String RESOURCE_PATH = System.getProperty("user.dir") + "/src/main/resources/";
+
+    public static String getProperty(String propertyName, String fileName){
+        Properties properties = null;
+        try {
+            properties = new Properties();
+            BufferedReader reader = new BufferedReader(new FileReader(RESOURCE_PATH + fileName));
+            properties.load(reader);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return properties.getProperty(propertyName);
+    }
+    public static void setProperty (String propertyFileName, String input, String outPut) throws IOException {
+
+        FileWriter fileWriter = new FileWriter("src/main/resources/" + propertyFileName);
+        Properties properties = new Properties();
+        properties.setProperty(input, outPut);
+        properties.store(fileWriter, null);
+    }
+    public static void setProperty (String propertyFileName, String input, int outPut) throws IOException {
+
+        FileWriter fileWriter = new FileWriter("src/main/resources/" + propertyFileName);
+        Properties properties = new Properties();
+        properties.setProperty(input, String.valueOf(outPut));
+        properties.store(fileWriter, null);
+    }
+}
